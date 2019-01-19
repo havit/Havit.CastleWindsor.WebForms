0bc32b2efe163314dc5f98c9f2575aba7db8a439
Ondřej Václavek <vaclavek@havit.cz>
2019-01-19 12:13:34 +0100

    Add Havit.CastleWindsor.WebForms

---
+++ src/Havit.CastleWindsor.WebForms/InjectAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Havit.CastleWindsor.WebForms
+{
+    /// <summary>
+    /// Marks properties that shall be injected in Web Services
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	public class InjectAttribute : Attribute
+	{
+	}
+}
---
+++ src/Havit.CastleWindsor.WebForms/InjectableWebServiceBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Services;
+
+namespace Havit.CastleWindsor.WebForms
+{
+    /// <summary>
+    /// Abstract base class for ASMX Web Services. It can have injected properties throght [Inject] attribute
+    /// </summary>
+    public abstract class InjectableWebServiceBase : WebService
+	{
+		/// <summary>
+        /// Constructor takes care of resolving Web Service dependencies
+		/// </summary>
+		protected InjectableWebServiceBase()
+		{
+			DependencyInjectionWebFormsHelper.InitializeInstance(this);
+		}
+
+		/// <summary>
+        /// We must release Web Service dependencies
+		/// </summary>
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+
+			if (disposing)
+			{
+				DependencyInjectionWebFormsHelper.ReleaseDependencies(this);
+			}
+		}
+	}
+}
