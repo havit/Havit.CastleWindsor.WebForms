e2cf61b303b7fb0700fbf2f175282377e7479625
Kanda, Jiří <kanda@havit.cz>
2019-01-30 09:47:36 +0100

    WebService (asmx) example

---
+++ src/Havit.CastleWindsor.WebForms.Example/WebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebService.asmx.cs" Class="Havit.CastleWindsor.WebForms.Example.WebService" %>
---
+++ src/Havit.CastleWindsor.WebForms.Example/WebService.asmx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Havit.CastleWindsor.WebForms.Example
+{
+	[WebService]
+	public class WebService : InjectableWebServiceBase
+	{
+		[Inject]
+		public IMyDependecy MyDependecy { get; set; }
+
+		[WebMethod]
+		public string SayHello()
+		{
+			return MyDependecy.MyMethod();
+		}
+	}
+}
