a802b53300732ab1f829e70df43f07fa4c107fea
Ondřej Václavek <vaclavek@havit.cz>
2020-02-07 17:55:23 +0100

    WebRequestScopeModule not initialized in global.asax start event

--- src/Havit.CastleWindsor.WebForms/Lifestyles/HybridPerWebRequestScopeScopeAccessor.cs
+++ src/Havit.CastleWindsor.WebForms/Lifestyles/HybridPerWebRequestScopeScopeAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Web;
 using Castle.Facilities.AspNet.SystemWeb;
 using Castle.MicroKernel.Context;
@@ -7,56 +8,62 @@ using Castle.MicroKernel.Lifestyle.Scoped;
 
 namespace Havit.CastleWindsor.WebForms.Lifestyles
 {
-	/// <summary>
-	/// Scope accessor for a hybrid lifestyle - per web request or per scope.
-	/// </summary>
-	public class HybridPerWebRequestScopeScopeAccessor : IScopeAccessor
-	{
-		private readonly IScopeAccessor webRequestScopeAccessor = new WebRequestScopeAccessor();
-		private readonly IScopeAccessor lifetimeScopeAccessor = new LifetimeScopeAccessor();
+    /// <summary>
+    /// Scope accessor for a hybrid lifestyle - per web request or per scope.
+    /// </summary>
+    public class HybridPerWebRequestScopeScopeAccessor : IScopeAccessor
+    {
+        private readonly IScopeAccessor webRequestScopeAccessor = new WebRequestScopeAccessor();
+        private readonly IScopeAccessor lifetimeScopeAccessor = new LifetimeScopeAccessor();
 
-		/// <inheritdoc />
-		public ILifetimeScope GetScope(CreationContext context)
-		{
-			if (HttpContext.Current != null)
-			{
-				return webRequestScopeAccessor.GetScope(context);
-			}
+        /// <inheritdoc />
+        public ILifetimeScope GetScope(CreationContext context)
+        {
+            if (HttpContext.Current != null && IsWebRequestModuleInitialized())
+            {
+                return webRequestScopeAccessor.GetScope(context);
+            }
 
-			return lifetimeScopeAccessor.GetScope(context);		
-		}
+            return lifetimeScopeAccessor.GetScope(context);
+        }
 
-		private bool disposed;
+        private bool disposed;
 
-		/// <summary>
-		/// Disposes scope accessor.
-		/// </summary>
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!disposed)
-			{
-				if (disposing)
-				{
-					webRequestScopeAccessor.Dispose();
-					lifetimeScopeAccessor.Dispose();
-				}
-				disposed = true;
-			}
-		}
+        /// <summary>
+        /// Disposes scope accessor.
+        /// </summary>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    webRequestScopeAccessor.Dispose();
+                    lifetimeScopeAccessor.Dispose();
+                }
+                disposed = true;
+            }
+        }
 
-		/// <inheritdoc />
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		/// <summary>
-		/// Finalizer.
-		/// </summary>
-		~HybridPerWebRequestScopeScopeAccessor()
-		{
-			Dispose(false);
-		}
-	}
+        /// <summary>
+        /// Finalizer.
+        /// </summary>
+        ~HybridPerWebRequestScopeScopeAccessor()
+        {
+            Dispose(false);
+        }
+
+        private static bool IsWebRequestModuleInitialized()
+        {
+            var fieldInfo = typeof(PerWebRequestLifestyleModule).GetField("initialized", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.GetField);
+            return (bool)fieldInfo.GetValue(null);
+        }
+    }
 }
